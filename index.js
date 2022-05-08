32bbf438e9f1204a39a8d63662c103996e965b6c
Guilherme Rodrigues <guilherme.ads.2022@gmail.com>
2022-05-08 12:23:05 -0300

    adicionando rota

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const sequelize = require('./infrastructure/configuration/connect-database');
-const {USER_REGISTRATION_MODEL} = require('./infrastructure/database/model/user-model/user-registration-model');
+
+const { USER_REGISTRATION_MODEL } = require('./infrastructure/database/model/user-model/user-registration-model');
 const app = express();
 USER_REGISTRATION_MODEL.sync();
-app.listen(3001, ()=> console.log("Server funcionando na porta 3001"))
+
+app.get('/', function(req,  res){
+    res.send('Hello Word');
+}) 
+
+
+
+app.listen(3001, () => console.log("Server funcionando na porta 3001"))
