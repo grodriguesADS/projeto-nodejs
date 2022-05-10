e7a38e4b4bb9ef07c43a61251433565c1976e446
Guilherme Rodrigues <guilherme.ads.2022@gmail.com>
2022-05-10 12:42:33 -0300

    arrumando rotas

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const sequelize = require('./infrastructure/configuration/connect-database');
+const router = require('../projeto-nodejs-iblue/routes/routes')
+const path = require('path');
+
 
 const { USER_REGISTRATION_MODEL } = require('./infrastructure/database/model/user-model/user-registration-model');
 const app = express();
 USER_REGISTRATION_MODEL.sync();
 
-app.get('/', function(req,  res){
-    res.send('Hello Word');
-}) 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use('/', router);
 
 app.listen(3001, () => console.log("Server funcionando na porta 3001"))
